1bc796e811724b8b37e6051119d3529b5adf649a
RodrigoFP51 <rodrigopizzinato@hotmail.com>
2024-05-05 22:57:09 -0300

    added gt table

--- script.R
+++ script.R
@@ -3,6 +3,7 @@ library(tidyquant)
 library(rvest)
 library(plotly)
 library(here)
+library(gt)
 
 if(!dir.exists("data")) dir.create("data")
 
@@ -19,7 +20,7 @@ tickers <- pag %>%
 #               "ABEV3", "LREN3", "CIEL3", "GGBR4", "WEGE3")
 
 get_yearly_yield <- function(tickers, from = Sys.Date() - 30,
-                             to = Sys.Date(), format_perc = FALSE){
+                             to = Sys.Date()){
   
   safe_tq_get <- safely(tq_get)
   
@@ -66,20 +67,24 @@ get_yearly_yield <- function(tickers, from = Sys.Date() - 30,
     ) %>% 
     mutate(yield = dividends / stock_price,
            symbol = str_remove_all(symbol, "\\.SA$")) %>% 
-    ungroup()    
-  #yield = ifelse(format_perc, round(yield * 100, digits =  2), yield))
+    ungroup()
   
   return(df)  
 }
 
 tictoc::tic()
-df <- get_yearly_yield(tickers[1:329], from = "2020-01-01") %>% 
+df <- get_yearly_yield(tickers[1:329], from = "2017-01-01") %>% 
   mutate(date = as.integer(date))
 tictoc::toc()
 
 df %>% 
   write_csv(here("data", "yield_data.csv"),
+            col_names = TRUE,
             append = TRUE)
+
+# df <-
+#   read_csv("data/yield_data.csv",
+#            col_names = c("symbol", "date", "stock_price", "dividend", "yield"))
 
 high_pay_tickers <- df %>% 
   group_by(symbol) %>%
@@ -91,10 +96,9 @@ high_pay_tickers <- df %>%
 
 df %>%
   filter(symbol %in% high_pay_tickers) %>% 
-  mutate(symbol = fct_reorder(symbol, -yield),
-         date = factor(date)) %>%  
+  mutate(symbol = fct_reorder(symbol, -yield)) %>%  
   ggplot(aes(date, yield)) +
-  geom_col(fill = "#459b45") +
+  geom_area(fill = "#459b45", alpha = 0.4, color = "#45cf49") +
   facet_wrap(vars(symbol), scales = "free")
 
 
@@ -103,12 +107,28 @@ df %>%
   group_by(symbol) %>% 
   summarize(first_yield = first(yield),
             last_yield = last(yield),
-            diff_yield = last(yield) - first(yield)) %>% 
-  arrange(desc(last_yield)) %>% 
-  knitr::kable(digits = 2,
-               align = "c",
-               col.names = c("Symbol", "First Yield", "Last Yield", "Diff Yield"))
-
+            diff_yield = last(yield) - first(yield),
+            min_date = min(date),
+            max_date = max(date)) %>% 
+  mutate(symbol = str_glue("{symbol} ({min_date}-{max_date})")) %>% 
+  select(-c(min_date, max_date)) %>% 
+  arrange(desc(diff_yield)) %>% 
+  gt() %>% 
+  cols_label_with(
+    columns = everything(),
+    fn = \(x) x %>% 
+      str_replace("_", " ") %>% 
+      str_to_title()
+  ) %>% 
+  fmt_percent(
+    columns = everything(),
+    decimals = 3
+  ) %>%
+  data_color(
+    columns = "diff_yield",
+    method = "numeric",
+    palette = "YlGn"
+  )
 
 df
 
